8453a378a254abacc2eebc497d00610727436b27
Mikko-P <parkkonenmikko@proton.me>
2024-11-22 15:03:38 +0200

    Added trackpad support

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ let isScrolling = false;
 function scrollToSection(index) {
     if (isScrolling) return;
     isScrolling = true;
-
-    sections[index].scrollIntoView({ behavior: "smooth" });
-
-    
+    sections[index].scrollIntoView({
+        behavior: "smooth"
+    });
     setTimeout(() => {
         isScrolling = false;
     }, 250);
-
     updateActiveNav(index);
 }
 
@@ -34,17 +32,15 @@ function handleScroll(e) {
         }, 200);
         return;
     }
-
     e.preventDefault();
-    
     if (isScrolling) return;
-
-    if (e.deltaY > 0) {
+    const threshold = 40;
+    if (e.deltaY > threshold) {
         if (currentSection < sections.length - 1) {
             currentSection++;
             scrollToSection(currentSection);
         }
-    } else {
+    } else if (e.deltaY < -threshold) {
         if (currentSection > 0) {
             currentSection--;
             scrollToSection(currentSection);
@@ -54,14 +50,11 @@ function handleScroll(e) {
 
 function handleTouchSwipe() {
     let startY;
-    
     window.addEventListener("touchstart", (e) => {
         startY = e.touches[0].clientY;
     });
-    
     window.addEventListener("touchend", (e) => {
         if (isZooming || isScrolling) return;
-        
         const endY = e.changedTouches[0].clientY;
         if (startY > endY + 50) {
             if (currentSection < sections.length - 1) {
@@ -86,9 +79,11 @@ window.addEventListener("resize", () => {
     }, 200);
 });
 
-window.addEventListener("wheel", handleScroll, { passive: false });
-handleTouchSwipe();
+window.addEventListener("wheel", handleScroll, {
+    passive: false
+});
 
+handleTouchSwipe();
 navItems.forEach((item, index) => {
     item.addEventListener("click", () => {
         currentSection = index;
